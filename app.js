4127176f2d1bb2c349a52b88ac9b63f8e5d0ee91
zhou <singzochn@gmail.com>
2023-03-02 19:29:43 +0800

    learn

--- app.js
+++ app.js
@@ -1,53 +1,42 @@
-const express = require('express')
-const mongoose = require('mongoose')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express()
-app.use(express.json())
-mongoose.connect('mongodb://127.0.0.1:27017/node_db')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const User = new mongoose.model(
-  'User',
-  new mongoose.Schema({
-    name: String,
-    age: Number,
-  })
-)
+var app = express();
 
-const createUser = ({ name, age }) =>
-  new User({ name, age }).save().then(() => `${name}创建成功，今年${age}岁`)
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.get('/', async (req, res) => {
-  const { name } = req.query
-  const query = await User.find(name ? { name } : {})
-  res.send(query)
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/p', express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'files')))
 
-app.post('/', (req, res) => {
-  const { name, age } = req.body
-  if (!name) {
-    res.send('请输入name')
-  }
-  if (!age) {
-    res.send('请输入age')
-  }
-  createUser({ name, age })
-    .then(r => res.send(r))
-    .catch(err => console.log('发生错误'))
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.put('/', async (req, res) => {
-  const { name, newOp } = req.body
-  if (!name) {
-    res.send('需要name')
-  }
-  await User.where({ name }).update(newOp)
-  res.send(`${name}完成更新，更新后为${newOp.name || name}`)
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-app.delete('/', async (req, res) => {
-  const { name } = req.body
-  const r = await User.deleteOne({ name })
-  res.send(r.deletedCount ? `成功删除${name}` : `删除失败`)
-})
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.listen(3000, () => console.log('node---启动'))
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
